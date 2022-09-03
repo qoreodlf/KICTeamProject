a262cef5a3b07fb04a22cd086b7cf7d704f02f6f
qoreodlf <qoreodlf37@naver.com>
2022-09-04 00:51:57 +0900

    chat test

--- teamProject/src/main/java/controller/ChatController.java
+++ teamProject/src/main/java/controller/ChatController.java
@@ -1,5 +1,25 @@
 package controller;
 
-public class ChatController {
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/chat")
+public class ChatController {
+	
+	HttpServletRequest request;
+	Model m;
+	HttpSession session;
+
+	@ModelAttribute
+	void init(HttpServletRequest request, Model m) {
+		this.request = request;
+		this.m = m;
+		this.session = request.getSession();
+	}
 }
